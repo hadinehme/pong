c4fd4d1e77108ca974e6a0cf82456d1b2260d30f
Hadi Nehme <hnehme@ad1.int.rtblw.com>
2023-06-20 17:04:39 +0200

    Adding Hexadecimal to binary conversion function

--- TestHelper.js
+++ TestHelper.js
@@ -190,3 +190,31 @@ function hexadecimalToText(hexadecimal) {
 
     return Buffer.from(hexadecimal, 'hex').toString('utf8');
 }
+
+function hexadecimalToBinary(hexadecimal) {
+    hexadecimal = hexadecimal.replace("0x", "").toLowerCase();
+    let binary = "";
+    for(let character of hexadecimal) {
+        switch(character) {
+            case '0': binary += "0000"; break;
+            case '1': binary += "0001"; break;
+            case '2': binary += "0010"; break;
+            case '3': binary += "0011"; break;
+            case '4': binary += "0100"; break;
+            case '5': binary += "0101"; break;
+            case '6': binary += "0110"; break;
+            case '7': binary += "0111"; break;
+            case '8': binary += "1000"; break;
+            case '9': binary += "1001"; break;
+            case 'a': binary += "1010"; break;
+            case 'b': binary += "1011"; break;
+            case 'c': binary += "1100"; break;
+            case 'd': binary += "1101"; break;
+            case 'e': binary += "1110"; break;
+            case 'f': binary += "1111"; break;
+            default: throw new Error('Input is not a hexadecimal number!');
+        }
+    }
+
+    return binary;
+}
